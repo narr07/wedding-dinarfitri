aafc4f7544f15ba0e6176cd5c5cf3cf6ebfd3fda
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-03-21 01:58:59 +0700

    Refactor index.vue and Galeri.vue components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
-import appCountDown from '~/components/appCountDown.vue';
+
 <template>
-  <div class="flex flex-col items-center h-screen justify-evenly">
+  <div class="flex flex-col items-center h-screen space-y-6 justify-evenly">
     <UCard class="flex flex-col items-center w-3/4 max-w-3xl mx-auto bgSvg justify-evenly">
       <h1 class="text-2xl text-center">
         Undangan Pernikahan
---
+++ storyblok/Listimage.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <div class="aspect-square ">
+    <NuxtImg class="object-cover aspect-square" :src="blok.image.filename" :alt="blok.title" />
+  </div>
+</template>
