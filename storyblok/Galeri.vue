e75681fdf832bc37f3349d1774d5196bc445947c
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-03-26 01:14:37 +0700

    Refactor Galeri.vue and Listimage.vue

--- storyblok/Galeri.vue
+++ storyblok/Galeri.vue
@@ -6,11 +6,11 @@ defineProps({ blok: Object })
   <UContainer v-editable="blok" class="py-10">
     <UCard>
       <div class="py-4">
-        <h1 class="text-2xl font-bold text-center">
+        <h1 class="py-4 text-2xl font-bold text-center">
           {{ blok.heading }}
         </h1>
       </div>
-      <div class="grid grid-cols-2 gap-2 md:grid-cols-4">
+      <div class="grid gap-2 md:grid-cols-4">
         <StoryblokComponent v-for="blok in blok.list" :key="blok._uid" :blok="blok" />
       </div>
     </UCard>
--- storyblok/Listimage.vue
+++ storyblok/Listimage.vue
@@ -3,7 +3,7 @@ defineProps({ blok: Object })
 </script>
 
 <template>
-  <div class="aspect-square ">
-    <NuxtImg class="object-cover aspect-square" :src="blok.image.filename" :alt="blok.title" />
+  <div class="bg-cover aspect-square">
+    <NuxtImg class="object-cover w-full h-64 bg-cover rounded-md aspect-square" :src="blok.image.filename" :alt="blok.title" />
   </div>
 </template>
